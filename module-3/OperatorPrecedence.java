43c434acf584ce102636b51938ec44c75346dda2
Sravanthi-1358 <srimanthulasravanthi1358@gmail.com>
2026-06-03 16:00:05 +0530

    Delete module-3 directory

--- module-3/OperatorPrecedence.java
+++
@@ -1,8 +0,0 @@
-public class OperatorPrecedence {
-    public static void main(String[] args) {
-        int result = 10 + 5 * 2 - 4 / 2;
-
-        System.out.println("Result = " + result);
-        System.out.println("Multiplication and division are performed before addition and subtraction.");
-    }
-}
